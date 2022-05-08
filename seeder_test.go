63e2ed40266e9486c231e906ab6bc44399aa1f6d
Kachit <antoxachaos@gmail.com>
2022-05-08 16:58:28 +0300

    initial

--- seeder_test.go
+++ seeder_test.go
@@ -2,6 +2,7 @@ package gorm_seeder
 
 import (
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/DATA-DOG/go-sqlmock.v1"
 	"regexp"
 	"testing"
 )
@@ -10,10 +11,22 @@ func Test_SeederAbstract_Delete(t *testing.T) {
 	db, mock := getDatabaseMock()
 	mock.MatchExpectationsInOrder(false)
 
-	mock.ExpectQuery(regexp.QuoteMeta(
-		`DELETE FROM foo`))
+	mock.ExpectExec(regexp.QuoteMeta(
+		`DELETE FROM foo`)).WillReturnResult(sqlmock.NewResult(0, 1))
 
 	sa := NewSeederAbstract(SeederConfiguration{})
 	err := sa.Delete(db, "foo")
+	assert.NoError(t, err)
+}
+
+func Test_SeederAbstract_Truncate(t *testing.T) {
+	db, mock := getDatabaseMock()
+	mock.MatchExpectationsInOrder(false)
+
+	mock.ExpectExec(regexp.QuoteMeta(
+		`TRUNCATE foo`)).WillReturnResult(sqlmock.NewResult(0, 1))
+
+	sa := NewSeederAbstract(SeederConfiguration{})
+	err := sa.Truncate(db, "foo")
 	assert.NoError(t, err)
 }
