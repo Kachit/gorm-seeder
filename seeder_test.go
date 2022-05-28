b9576e7e0a1744e4e0fd4055d58e95f503698e2c
Kachit <antoxachaos@gmail.com>
2022-05-28 23:38:03 +0300

    fix test

--- seeder_test.go
+++ seeder_test.go
@@ -2,117 +2,127 @@ package gorm_seeder
 
 import (
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"gopkg.in/DATA-DOG/go-sqlmock.v1"
+	"gorm.io/gorm"
 	"regexp"
 	"testing"
 )
 
-func Test_SeederAbstract_Delete(t *testing.T) {
+type SeederAbstract_TestSuite struct {
+	suite.Suite
+	db       *gorm.DB
+	mock     sqlmock.Sqlmock
+	testable SeederAbstract
+}
+
+func (suite *SeederAbstract_TestSuite) SetupTest() {
 	db, mock := getDatabaseMock()
 	mock.MatchExpectationsInOrder(false)
+	suite.db = db
+	suite.mock = mock
+	suite.testable = NewSeederAbstract(SeederConfiguration{})
+}
 
-	mock.ExpectExec(regexp.QuoteMeta(
+func (suite *SeederAbstract_TestSuite) TestDelete() {
+	suite.mock.ExpectExec(regexp.QuoteMeta(
 		`DELETE FROM foo`)).WillReturnResult(sqlmock.NewResult(0, 1))
 
 	sa := NewSeederAbstract(SeederConfiguration{})
-	err := sa.Delete(db, "foo")
-	assert.NoError(t, err)
+	err := sa.Delete(suite.db, "foo")
+	assert.NoError(suite.T(), err)
 }
 
-func Test_SeederAbstract_Truncate(t *testing.T) {
-	db, mock := getDatabaseMock()
-	mock.MatchExpectationsInOrder(false)
-
-	mock.ExpectExec(regexp.QuoteMeta(
+func (suite *SeederAbstract_TestSuite) TestTruncate() {
+	suite.mock.ExpectExec(regexp.QuoteMeta(
 		`TRUNCATE foo`)).WillReturnResult(sqlmock.NewResult(0, 1))
 
 	sa := NewSeederAbstract(SeederConfiguration{})
-	err := sa.Truncate(db, "foo")
-	assert.NoError(t, err)
+	err := sa.Truncate(suite.db, "foo")
+	assert.NoError(suite.T(), err)
 }
 
-func Test_SeedersStack_AddSeeder(t *testing.T) {
-	ss := NewSeedersStack(nil)
-	assert.Len(t, ss.seeders, 0)
-	ss.AddSeeder(NewUsersSeederV1(SeederConfiguration{}))
-	assert.Len(t, ss.seeders, 1)
+func Test_SeederAbstract_TestSuite(t *testing.T) {
+	suite.Run(t, new(SeederAbstract_TestSuite))
 }
 
-func Test_SeedersStack_BeginTransaction(t *testing.T) {
-	db, mock := getDatabaseMock()
-	db.SkipDefaultTransaction = true
-	mock.MatchExpectationsInOrder(false)
-	ss := NewSeedersStack(db)
-	ss.beginTransaction(db)
+type SeedersStack_TestSuite struct {
+	suite.Suite
+	db       *gorm.DB
+	mock     sqlmock.Sqlmock
+	testable *SeedersStack
 }
 
-func Test_SeedersStack_CommitTransaction(t *testing.T) {
-	db, mock := getDatabaseMock()
-	db.SkipDefaultTransaction = true
-	mock.MatchExpectationsInOrder(false)
-	ss := NewSeedersStack(db)
-	ss.commitTransaction(db)
-}
-
-func Test_SeedersStack_RollbackTransaction(t *testing.T) {
-	db, mock := getDatabaseMock()
-	db.SkipDefaultTransaction = true
-	mock.MatchExpectationsInOrder(false)
-	ss := NewSeedersStack(db)
-	ss.rollbackTransaction(db)
-}
-
-func Test_SeedersStack_SeedInBatches(t *testing.T) {
+func (suite *SeedersStack_TestSuite) SetupTest() {
 	db, mock := getDatabaseMock()
 	mock.MatchExpectationsInOrder(false)
-	mock.ExpectBegin()
-	mock.ExpectQuery(regexp.QuoteMeta(
+	suite.db = db
+	suite.mock = mock
+	suite.testable = NewSeedersStack(db)
+}
+
+func (suite *SeedersStack_TestSuite) TestAddSeeder() {
+	assert.Len(suite.T(), suite.testable.seeders, 0)
+	suite.testable.AddSeeder(NewUsersSeederV1(SeederConfiguration{}))
+	assert.Len(suite.T(), suite.testable.seeders, 1)
+}
+
+func (suite *SeedersStack_TestSuite) TestBeginTransaction() {
+	suite.db.SkipDefaultTransaction = true
+	suite.testable.beginTransaction(suite.db)
+}
+
+func (suite *SeedersStack_TestSuite) TestCommitTransaction() {
+	suite.db.SkipDefaultTransaction = true
+	suite.testable.commitTransaction(suite.db)
+}
+
+func (suite *SeedersStack_TestSuite) TestRollbackTransaction() {
+	suite.db.SkipDefaultTransaction = true
+	suite.testable.rollbackTransaction(suite.db)
+}
+
+func (suite *SeedersStack_TestSuite) TestSeedInBatches() {
+	suite.mock.ExpectBegin()
+	suite.mock.ExpectQuery(regexp.QuoteMeta(
 		`INSERT INTO "users" ("name","email","password","created_at") VALUES ($1,$2,$3,$4),($5,$6,$7,$8),($9,$10,$11,$12),($13,$14,$15,$16),($17,$18,$19,$20) RETURNING "id"`)).
-		//WithArgs(1, 2).
 		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
 
-	mock.ExpectCommit()
-	ss := NewSeedersStack(db)
-	ss.AddSeeder(NewUsersSeederV1(SeederConfiguration{Rows: 5}))
-	err := ss.Seed()
-	assert.NoError(t, err)
+	suite.mock.ExpectCommit()
+	suite.testable.AddSeeder(NewUsersSeederV1(SeederConfiguration{Rows: 5}))
+	err := suite.testable.Seed()
+	assert.NoError(suite.T(), err)
 }
 
-func Test_SeedersStack_ClearDelete(t *testing.T) {
-	db, mock := getDatabaseMock()
-	mock.MatchExpectationsInOrder(false)
-	mock.ExpectExec(regexp.QuoteMeta(
-		`DELETE FROM users`)).WillReturnResult(sqlmock.NewResult(0, 1))
-	ss := NewSeedersStack(db)
-	ss.AddSeeder(NewUsersSeederV1(SeederConfiguration{Rows: 5}))
-	err := ss.Clear()
-	assert.NoError(t, err)
-}
-
-func Test_SeedersStack_SeedInLoopWithTransaction(t *testing.T) {
-	db, mock := getDatabaseMock()
-	db.SkipDefaultTransaction = true
-	mock.MatchExpectationsInOrder(false)
-	mock.ExpectBegin()
-	mock.ExpectQuery(regexp.QuoteMeta(
+func (suite *SeedersStack_TestSuite) TestSeedInLoopWithTransaction() {
+	suite.db.SkipDefaultTransaction = true
+	suite.mock.ExpectBegin()
+	suite.mock.ExpectQuery(regexp.QuoteMeta(
 		`INSERT INTO "users" ("name","email","password","created_at") VALUES ($1,$2,$3,$4) RETURNING "id"`)).
-		//WithArgs(1, 2).
 		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
 
-	mock.ExpectCommit()
-	ss := NewSeedersStack(db)
-	ss.AddSeeder(NewUsersSeederV2(SeederConfiguration{Rows: 1}))
-	err := ss.Seed()
-	assert.NoError(t, err)
+	suite.mock.ExpectCommit()
+	suite.testable.AddSeeder(NewUsersSeederV2(SeederConfiguration{Rows: 1}))
+	err := suite.testable.Seed()
+	assert.NoError(suite.T(), err)
 }
 
-func Test_SeedersStack_ClearTruncate(t *testing.T) {
-	db, mock := getDatabaseMock()
-	mock.MatchExpectationsInOrder(false)
-	mock.ExpectExec(regexp.QuoteMeta(
+func (suite *SeedersStack_TestSuite) TestClearDelete() {
+	suite.mock.ExpectExec(regexp.QuoteMeta(
+		`DELETE FROM users`)).WillReturnResult(sqlmock.NewResult(0, 1))
+	suite.testable.AddSeeder(NewUsersSeederV1(SeederConfiguration{Rows: 5}))
+	err := suite.testable.Clear()
+	assert.NoError(suite.T(), err)
+}
+
+func (suite *SeedersStack_TestSuite) TestClearTruncate() {
+	suite.mock.ExpectExec(regexp.QuoteMeta(
 		`TRUNCATE users`)).WillReturnResult(sqlmock.NewResult(0, 1))
-	ss := NewSeedersStack(db)
-	ss.AddSeeder(NewUsersSeederV2(SeederConfiguration{Rows: 5}))
-	err := ss.Clear()
-	assert.NoError(t, err)
+	suite.testable.AddSeeder(NewUsersSeederV2(SeederConfiguration{Rows: 5}))
+	err := suite.testable.Clear()
+	assert.NoError(suite.T(), err)
+}
+
+func Test_SeedersStack_TestSuite(t *testing.T) {
+	suite.Run(t, new(SeedersStack_TestSuite))
 }
